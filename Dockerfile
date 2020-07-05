a91b78d3be3f73cba6afed058c221dab31cde918
Jelle Besseling <jelle@pingiun.com>
2020-07-05 14:42:25 +0200

    Add autoupdate

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,8 @@ RUN apk add --no-cache tini iptables ip6tables curl
 
 ADD configure-firewall.sh /bin
 
-RUN curl https://www.spamhaus.org/drop/drop.txt 2> /dev/null | sed 's/;.*//' > /drop.txt
-RUN curl https://www.spamhaus.org/drop/dropv6.txt 2> /dev/null | sed 's/;.*//' > /dropv6.txt
+RUN curl https://www.spamhaus.org/drop/drop.txt 2> /dev/null | sed 's/;.*//' > /lists/drop.txt
+RUN curl https://www.spamhaus.org/drop/dropv6.txt 2> /dev/null | sed 's/;.*//' > /lists/dropv6.txt
 
 ENTRYPOINT ["/sbin/tini", "--"]
 
--- configure-firewall.sh
+++ configure-firewall.sh
@@ -17,6 +17,14 @@ cleanup() {
   exit 0
 }
 
+# Redownload the lists if they are at least a day old
+if [ $(stat -c%Y /lists/drop.txt) -le $(( $(date +%s) - 86400 )) ]; then
+    curl https://www.spamhaus.org/drop/drop.txt 2> /dev/null | sed 's/;.*//' > /lists/drop.txt
+fi
+if [ $(stat -c%Y /lists/dropv6.txt) -le $(( $(date +%s) - 86400 )) ]; then
+    curl https://www.spamhaus.org/drop/dropv6.txt 2> /dev/null | sed 's/;.*//' > /lists/dropv6.txt
+fi
+
 trap cleanup TERM
 
 ip46tables -D INPUT -j blocklist 2> /dev/null || true
@@ -26,13 +34,13 @@ ip46tables -X blocklist 2> /dev/null || true
 ip46tables -N blocklist
 
 echo "Configuring IPv4 blocklist..."
-for ip in $(cat /drop.txt); do
+for ip in $(cat /lists/drop.txt); do
   iptables -A blocklist -s $ip -j DROP
 done
 iptables -A blocklist -j RETURN
 
 echo "Configuring IPv6 blocklist..."
-for ip in $(cat /dropv6.txt); do
+for ip in $(cat /lists/dropv6.txt); do
   ip6tables -A blocklist -s $ip -j DROP
 done
 ip6tables -A blocklist -j RETURN
